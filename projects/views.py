e65c691f67370404b4a3364ad66bed43fe3148a6
mazimpakar <rosemazimpaka2@gmail.com>
2019-04-01 14:16:38 +0200

    add login_required

--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,61 @@
-from django.shortcuts import render
-from django.http  import HttpResponse
+from django.shortcuts import render,redirect
+from django.http import HttpResponse, Http404,HttpResponseRedirect
 import datetime as dt
+from .models import Projects 
+from .forms import NewProjectsForm
+# from .email import send_welcome_email
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
-def welcome(request):
-     return render(request, 'welcome.html')
-def projects_of_day(request):
+@login_required(login_url='/accounts/login/')
+def projects_today(request):
     date = dt.date.today()
-    return render(request, 'today_projects.html', {"date": date,})
+    all_projects = Projects.all_projects()
+    projects= Projects.objects.all()
+    print(projects)
+    
+#     if request.method == 'POST':
+        
+        
+#         form = NewProjectsForm()
+    return render(request, 'today_projects.html', {"date": date, "projects":all_projects})
+
+
+
+
+def search_results(request):
+
+    if 'projects' in request.GET and request.GET["projects"]:
+        search_term = request.GET.get("projects")
+        searched_projects = Projects.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"projects": searched_projects})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
+
+@login_required(login_url='/accounts/login/')
+def projects(request,projects_id):
+    try:
+        project= Projects.objects.get(id = projects_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"projects.html", {"projects":projects})
+
+@login_required(login_url='/accounts/login/')
+def new_projects(request):
+    current_user = request.user
+    title = 'New projects'
+    if request.method == 'POST':
+        form = NewProjectsForm(request.POST, request.FILES)
+        if form.is_valid():
+            projects = form.save(commit=False)
+            projects.user = current_user
+            projects.save()
+        return redirect('todaysProjects')
+
+    else:
+        form = NewProjectsForm()
+    return render(request, 'new_projects.html', {"form": form,"current_user":current_user,"title":title})
