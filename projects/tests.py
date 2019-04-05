667e471870f7ab9a9b07c3fb9adba0fe7b119388
mazimpakar <rosemazimpaka2@gmail.com>
2019-04-05 08:46:56 +0200

    add test for save method

--- projects/tests.py
+++ projects/tests.py
@@ -41,9 +41,9 @@ class ProfileTestClass(TestCase):
     def test_instance(self):
         self.assertTrue(isinstance(self.rose,Profile))
 
-#     # Testing Save Method
-#     def test_save_method(self):
-#         self.rose.save_profile()
-#         image = Profile.objects.all()
-#         self.assertTrue(len(image) > 0) 
+    # Testing Save Method
+    def test_save_method(self):
+        self.rose.save_profile()
+        image = Profile.objects.all()
+        self.assertTrue(len(image) > 0) 
     
